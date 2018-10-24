4461c7a5ab5c887342cd77a1e07928a5a5dac0b0
Moesha Amos <moemusiclover7@gmail.com>
2018-10-23 20:07:22 -0700

    Exercise 2 completed

--- maze.js
+++ maze.js
@@ -1,16 +1,36 @@
+//Lab4 Done by Moesha Amos
 
 	
-	//Part 1
+	//Ex 1
 	var bound1;
 	
-	window.onload = topLeftBound;
+	window.onload = allBounds;/*topLeftBound;*/
 	function topLeftBound(){
 		bound1 = document.getElementById("boundary1");
 		bound1.onmouseover = turnRed;
 	}
 	
 	
-	function turnRed(){
-		bound1.classList.add("youlose");
-	}
+	
 
+//Ex 2
+
+//var size;
+//var bounds;
+//var i;
+
+function allBounds(){
+	var bounds = document.querySelectorAll(".boundary");
+	//size = bounds.length;
+	for(let i=0; i<bounds.length;i++){
+		bounds[i].addEventListener("mouseover",turnRed);
+	}
+}
+
+function turnRed(){
+		touch = true;
+		var bounds= document.querySelectorAll(".boundary");
+		for(let i=0; i<bounds.length; i++){
+			bounds[i].classList.add("youlose");
+		}
+}
